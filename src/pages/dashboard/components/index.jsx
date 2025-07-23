2cf09c49ac04738dbf3d14ed94c4ae99d0c63967
jetheesh <jetheesh.ikm@kerala.gov.in>
2025-07-23 08:44:49 +0530

    Admin User Reports and Dashboard

--- src/pages/dashboard/components/index.jsx
+++ src/pages/dashboard/components/index.jsx
@@ -8,17 +8,19 @@ import { STORAGE_KEYS } from 'pages/common/constants';
 import Breadcrumbs from 'pages/common/components/Breadcrumbs';
 import { _ } from 'utils/lodash';
 import JoinMedsLoader from 'pages/common/components/JoinMedsLoader';
+import ReportTable from 'pages/common/components/ReportTable';
+import { formatDate } from 'utils/date';
 
 function Dashboard() {
   const dispatch = useDispatch();
   const dashBoardDetails = useSelector(getDashBoardInfo);
   const { id = '', userType = '' } = getDataFromStorage(STORAGE_KEYS.OFFICE_DETAILS, true) || {};
   const [jobList, setJobList] = useState([]);
-  const [loading, setLoading] = useState(true); 
-  
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     if (!_.isEmpty(id)) {
-      setLoading(true); // Start loading
+      setLoading(true);
       if (userType === 'SUPERADMIN') {
         dispatch(fetchAdminDashboardInfo());
       } else {
@@ -30,9 +32,27 @@ function Dashboard() {
   useEffect(() => {
     if (!_.isEmpty(dashBoardDetails)) {
       setJobList(dashBoardDetails);
-      setLoading(false); // Stop loading once data is ready
+      setLoading(false);
     }
   }, [dashBoardDetails]);
+
+
+  const columns = [
+  { key: 'orgName', label: 'Organization Name' },
+    { key: 'hiringFor', label: 'Hiring For' },
+    { key: 'yearExp', label: 'Year of Experience' },
+    { key: 'skills', label: 'Skills' },
+    { key: 'natureJob', label: 'Nature of Job' },
+    { key: 'payFrom', label: 'Pay From' },
+    { key: 'payTo', label: 'Pay To' },
+    { key: 'payRange', label: 'Pay Range' },
+    { key: 'jobDesc', label: 'Job Description' },
+    {
+      key: 'createdAt',
+      label: 'Publsihed On',
+      render: (value) => formatDate(value),
+    }
+  ];
 
   return (
     <div className="p-6">
@@ -47,11 +67,22 @@ function Dashboard() {
           <p className="text-gray-500">There are currently no job postings available. Please check back later!</p>
         </div>
       ) : (
-        <div className="flex flex-wrap gap-4">
-          {jobList.map((job, index) => (
-            <JobCard key={index} {...job} dashBoardDetails={dashBoardDetails} jobId={job?.id} />
-          ))}
-        </div>
+        <>
+          {userType === 'SUPERADMIN' ? (
+            <ReportTable
+              title="Applied Jobs"
+              columns={columns}
+              data={jobList}
+              rowsPerPage={10}
+            />
+          ) : (
+            <div className="flex flex-wrap gap-4">
+              {jobList.map((job, index) => (
+                <JobCard key={index} {...job} dashBoardDetails={dashBoardDetails} jobId={job?.id} />
+              ))}
+            </div>
+          )}
+        </>
       )}
     </div>
   );
